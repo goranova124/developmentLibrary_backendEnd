c9f3a1d505fb13db417a146f3f778e17b4c9a68d
Denitsa Goranova <Denitsa.Goranova@DAFTRUCKS.com>
2024-06-26 11:36:45 +0200

    Updated filed for CORS

--- src/main/java/org/example/controller/ApiController.java
+++ src/main/java/org/example/controller/ApiController.java
@@ -13,19 +13,19 @@ import java.sql.SQLException;
 
 
 @RestController
-@CrossOrigin(origins = {"https://gentle-cliff-093aa731e.5.azurestaticapps.net/","www.paccarconnectdeveloperlibrary.com"},allowedHeaders = "*")
+@CrossOrigin(origins = {"https://gentle-cliff-093aa731e.5.azurestaticapps.net/","https://www.paccarconnectdeveloperlibrary.com","https://paccarconnectdeveloperlibrary.com"},allowedHeaders = "*")
 @RequestMapping("/apis")
 @AllArgsConstructor
 public class ApiController {
     private final GetAPIUseCase getAPIsUseCase;
     private final GetAPIDetailsUseCase getAPIUseCase;
-    @CrossOrigin(origins = {"https://gentle-cliff-093aa731e.5.azurestaticapps.net/","www.paccarconnectdeveloperlibrary.com"})
+    @CrossOrigin(origins = {"https://gentle-cliff-093aa731e.5.azurestaticapps.net/","https://www.paccarconnectdeveloperlibrary.com","https://paccarconnectdeveloperlibrary.com"})
     @GetMapping
     public ResponseEntity<Object> getApis() throws SQLException {
         GetAPISResponse api = getAPIsUseCase.getApiResponse();
         return ResponseEntity.status(HttpStatus.OK).body(api);
     }
-    @CrossOrigin(origins = {"https://gentle-cliff-093aa731e.5.azurestaticapps.net/","www.paccarconnectdeveloperlibrary.com"})
+    @CrossOrigin(origins = {"https://gentle-cliff-093aa731e.5.azurestaticapps.net/","https://www.paccarconnectdeveloperlibrary.com","https://paccarconnectdeveloperlibrary.com"})
     @GetMapping("/detailsApi")
     public ResponseEntity<Object> getApiByName(@RequestParam String name) throws SQLException {
         GetAPIDetailsResponse api = getAPIUseCase.getAPIDetails(name);
